9d24867c8c0dd29147a15c9171e64f6ec9fdd291
snipe <72265661+notsniped@users.noreply.github.com>
2022-10-28 23:30:16 +0530

    Make `MessageHandler` object for handling and sending messages through the API

--- messages.py
+++ messages.py
@@ -2,4 +2,35 @@
 
 """Handles and sends messages through RqChat."""
 
-# TODO: Make send message command and reply message command
+import requests
+import datetime
+import time
+
+
+class MessageHandler:
+    def __init__(self, username: str, client_id, *, show_timestamps: bool = False):
+        self.username = username
+        self.__client_id = client_id
+        self.show_timestamps = show_timestamps
+
+    def send(self, message: str):
+        m = str()
+        if self.show_timestamps:
+            ts = datetime.datetime.now()
+            ts_formatted = time.strftime("%H:%M", ts)
+            m = f"[{ts_formatted}] {message}"
+        else: m = message
+        api_request = requests.get(f"https://rqchat.thatonearchuser.repl.co/send?username={self.username}&msg={m}&cid={self.__client_id}")
+        if api_request.status_code != 200: raise RuntimeError(f"Message send failure (Status Code {api_request.status_code}): {api_request.text}")
+        else: return True
+
+    def reply(self, base_message_content: str, message: str):
+        m = str()
+        if self.show_timestamps:
+            ts = datetime.datetime.now()
+            ts_formatted = time.strftime("%H:%M", ts)
+            m = f"[{ts_formatted}] > {base_message_content}: {message}"
+        else: m = f"> {base_message_content}: {message}"
+        api_request = requests.get(f"https://rqchat.thatonearchuser.repl.co/send?username={self.username}&msg={m}&cid={self.__client_id}")
+        if api_request.status_code != 200: raise RuntimeError(f"Message send failure (Status Code {api_request.status_code}): {api_request.text}")
+        else: return True
