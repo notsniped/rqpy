2e0264aa55a02017677dccbf62370ded33110c3d
snipe_blaze <72265661+notsniped@users.noreply.github.com>
2022-11-01 10:49:18 +0530

    Move command listener into a class object `PrefixCommandListener()`

--- example_bot.py
+++ example_bot.py
@@ -20,43 +20,47 @@ ctx = MessageHandler(username, client_id)
 channel = ChatListener()
 
 # Commands
-def command_listener():
+class PrefixCommandListener:
     """Handles commands in the bot client."""
 
-    latest_message_cached = ""
+    def start(self):
+        """Starts listening for commands"""
 
-    def wait_for_message():
-        latest_message_context = latest_message.split(": ")
-        latest_message_author = latest_message_context[0]
-        latest_message_content = latest_message_context[1]
-        if latest_message_author == username: return
-        print(latest_message_content)
-        if str(latest_message_content) == "/say_hi": 
-            ctx.send("Hellow uwu")
-            latest_message_cached = ""
-            return
-        elif str(latest_message_content) == "/help": 
-            ctx.send("Help list:\n    /help (this command): sends help\n    /say_hi: makes the bot say hi back to you")
-            latest_message_cached = ""
-            return
-        else:
-            latest_message_cached = "" 
-            return
+        latest_message_cached = ""
 
-    while True:
-        latest_message = channel.get_message()
-        if latest_message != latest_message_cached or len(latest_message) != 0: 
-            latest_message_cached = latest_message
-            wait_for_message()
-        else: 
-            pass
+        def wait_for_message():
+            latest_message_context = latest_message.split(": ")
+            latest_message_author = latest_message_context[0]
+            latest_message_content = latest_message_context[1]
+            if latest_message_author == username: return
+            print(latest_message_content)
+            if str(latest_message_content) == "/say_hi": 
+                ctx.send("Hellow uwu")
+                latest_message_cached = ""
+                return
+            elif str(latest_message_content) == "/help": 
+                ctx.send("Help list:\n    /help (this command): sends help\n    /say_hi: makes the bot say hi back to you")
+                latest_message_cached = ""
+                return
+            else:
+                latest_message_cached = "" 
+                return
 
-        # NOTE:
-        # Avoid using latest message cache for 
-        # triggering bot commands. Doing this 
-        # may lead to multiple commands being
-        # false-invoked at the same time. Use 
-        # direct message cache instead.
+        while True:
+            latest_message = channel.get_message()
+            if latest_message != latest_message_cached or len(latest_message) != 0: 
+                latest_message_cached = latest_message
+                wait_for_message()
+            else: 
+                pass
+
+            # NOTE:
+            # Avoid using latest message cache for 
+            # triggering bot commands. Doing this 
+            # may lead to multiple commands being
+            # false-invoked at the same time. Use 
+            # direct message cache instead.
 
 # Running Command Listener
-command_listener()
+commands = PrefixCommandListener()
+commands.start()
